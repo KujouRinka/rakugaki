ee487f8b5dce82b4d28f61b6664207691719f9bb
KujouRinka <breakhopec@gmail.com>
2021-04-02 16:49:22 +0800

    10.3.3

---
+++ C++Primer/c10/10.3.2.cpp
@@ -0,0 +1,15 @@
+//
+// Created by KujouRinka on 2021/4/2.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int someLambda() {
+    int cap = 1;
+    auto f1 = [](int a, int b) -> int { return a + b};
+    auto f2 = [cap](int a) -> int { return cap + a };
+    
+    return 0;
+}
