f657fe4c3655b52ebc95798935dedcaa2239f613
KujouRinka <kujourinka@gmail.com>
2021-05-16 22:33:11 +0800

    everyday practice

---
+++ C++Primer/c14/14.5&8&12.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+class Date {
+
+    friend std::ostream &operator<<(std::ostream &os, const Date &rhs);
+
+    friend std::istream &operator>>(std::istream &is, Date &rhs);
+
+    friend bool operator==(Date &lhs, Date &rhs);
+
+public:
+
+    Date(const int &y = 0, const int &m = 0, const int &d = 0) :
+            year(y), month(m), day(d) {}
+
+private:
+
+    int year, month, day;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const Date &rhs) {
+    std::string sep = "\t";
+    os << "year: " << rhs.year
+       << sep << "month: " << rhs.month
+       << sep << "day: " << rhs.day;
+    return os;
+}
+
+std::istream &operator>>(std::istream &is, Date &rhs) {
+    is >> rhs.year >> rhs.month >> rhs.day;
+    if (!is)
+        rhs = Date();
+    return is;
+}
+
+bool operator==(Date &lhs, Date &rhs) {
+    return lhs.year == rhs.year && lhs.month == rhs.month && lhs.day == rhs.day;
+}
