6fb45a6bdb81d8945d60a391a050ed3f6d40a3b0
KujouRinka <kujourinka@gmail.com>
2021-05-25 21:46:02 +0800

    feat: add Basket class

--- C++Primer/c15/15.3&11&4.x.cpp
+++ C++Primer/c15/15.3&11&4.x.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <memory>
+#include <set>
 
 class Quote {
 public:
@@ -7,6 +10,9 @@ public:
             bookNo(book), price(sales_price) {}
     Quote(const Quote &rhs) :
             bookNo(rhs.bookNo), price(rhs.price) {}
+    Quote(Quote &&rhs) = default;
+    virtual Quote *clone() const &{ return new Quote(*this); }
+    virtual Quote *clone() &&{ return new Quote(std::move(*this)); }
 
     Quote &operator=(const Quote &rhs) {
         bookNo = rhs.bookNo;
@@ -37,6 +43,7 @@ public:
             Quote(book, price), quantity(qty), discount(disc) {}
     Disc_quote(const Disc_quote &rhs) :
             Quote(rhs), quantity(rhs.quantity), discount(rhs.discount) {}
+    Disc_quote(Disc_quote &&rhs) = default;
 
     Disc_quote &operator=(const Disc_quote &rhs) {
         Quote::operator=(rhs);
@@ -62,6 +69,9 @@ public:
     using Disc_quote::Disc_quote;
     Bulk_quote(const Bulk_quote &rhs) :
             Disc_quote(rhs) {}
+    Bulk_quote(Bulk_quote &&rhs) = default;
+    Bulk_quote *clone() const & override { return new Bulk_quote(*this); }
+    Bulk_quote *clone() && override { return new Bulk_quote(std::move(*this)); }
 
     Bulk_quote &operator=(const Bulk_quote &rhs) {
         Disc_quote::operator=(rhs);
@@ -91,6 +101,9 @@ public:
     using Disc_quote::Disc_quote;
     Limited_quote(const Limited_quote &rhs) :
             Disc_quote(rhs) {}
+    Limited_quote(Limited_quote &&rhs) = default;
+    Limited_quote *clone() const & override { return new Limited_quote(*this); }
+    Limited_quote *clone() && override { return new Limited_quote(std::move(*this)); }
 
     Limited_quote &operator=(const Limited_quote &rhs) {
         Disc_quote::operator=(rhs);
@@ -117,12 +130,49 @@ double print_total(std::ostream &os, const Quote &item, std::size_t n) {
     return ret;
 }
 
+class Basket {
+public:
+    Basket() = default;
+
+    void add_item(const Quote &sale) {
+        items.insert(std::shared_ptr<Quote>(sale.clone()));
+    }
+    void add_item(Quote &&sale) {
+        items.insert(std::shared_ptr<Quote>(std::move(sale).clone()));
+    }
+    void add_item(const std::shared_ptr<Quote> &sale) {
+        items.insert(sale);
+    }
+    double total_receipt(std::ostream &os) const;
+
+private:
+    static bool compare(const std::shared_ptr<Quote> &lhs,
+                        const std::shared_ptr<Quote> &rhs) {
+        return lhs->isbn() < rhs->isbn();
+    }
+
+    std::multiset<std::shared_ptr<Quote>, decltype(compare) *> items{compare};
+};
+
+double Basket::total_receipt(std::ostream &os) const {
+    double sum = 0.0;
+    for (auto it = items.cbegin(); it != items.cend(); it = items.upper_bound(*it))
+        sum += print_total(os, **it, items.count(*it));
+    os << "Total Sale: " << sum << std::endl;
+    return sum;
+}
+
 int main() {
     Quote b1("123456-2345", 30.5);
     Bulk_quote b2("114514-2333", 30.5, 50, 0.2);
     Limited_quote b3("170001-1024", 30.5, 50, 0.2);
-    print_total(std::cout, b1, 100);
-    print_total(std::cout, b2, 100);
-    print_total(std::cout, b3, 100);
+    Basket basket;
+    for (int i = 0; i < 100; ++i) {
+        basket.add_item(b1);
+        basket.add_item(b2);
+        basket.add_item(b3);
+    }
+    basket.total_receipt(std::cout);
+
     return 0;
 }
