47a5a9f287de66fef02db67201d532096d1ebfd1
KujouRinka <kujourinka@gmail.com>
2021-09-05 16:48:53 +0800

    leetcode everyday

---
+++ LeetCode/470.cpp
@@ -0,0 +1,14 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        while (true) {
+            int n = (rand7() - 1) * 7 + (rand7() - 1);
+            if (n >= 1 && n <= 40)
+                return n % 10 + 1;
+        }
+    }
+};
