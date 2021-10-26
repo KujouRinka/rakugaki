66a234f46a5ac2fb9a1d591c869b24d0a3c1b6f7
kujourinka <kujourinka@gmail.com>
2021-10-26 20:19:18 +0800

    leetcode everyday

---
+++ LeetCode/p496.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
+        unordered_map<int, int> check_map;
+        stack<int> incr_stack;
+
+        for (auto rit = nums2.rbegin(); rit != nums2.rend(); ++rit) {
+            while (true) {
+                if (incr_stack.empty()) {
+                    check_map[*rit] = -1;
+                    incr_stack.push(*rit);
+                    break;
+                }
+                if (incr_stack.top() < *rit) {
+                    incr_stack.pop();
+                } else {
+                    check_map[*rit] = incr_stack.top();
+                    incr_stack.push(*rit);
+                    break;
+                }
+            }
+        }
+
+        vector<int> result;
+        result.reserve(nums1.size());
+        for (int i: nums1)
+            result.push_back(check_map[i]);
+        return result;
+    }
+};
