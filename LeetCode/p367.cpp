38dbd22cec2646bb58db3668bc8ee2b6991f4b12
kujourinka <kujourinka@gmail.com>
2021-11-04 16:28:17 +0800

    leetcode everyday

---
+++ LeetCode/p367.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long int i = 0;
+        while (i * i <= num) {
+            if (i * i == num)
+                return true;
+            ++i;
+        }
+        return false;
+    }
+};
