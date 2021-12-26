4ec4ca5534e6149575cb1c7d398be91b34f90e4e
KujouRinka <kujourinka@gmail.com>
2021-12-26 23:24:15 +0800

    p349

---
+++ LeetCode/p349.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> intersection(const vector<int> &nums1, const vector<int> &nums2) {
+        set<int> s1(nums1.begin(), nums1.end());
+        set<int> s2(nums2.begin(), nums2.end());
+        vector<int> result;
+        set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), back_inserter(result));
+        return result;
+    }
+};
+
