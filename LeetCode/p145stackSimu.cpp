d785fc5726354d6e6222484c4cde122cdbe920a0
KujouRinka <kujourinka@gmail.com>
2021-12-29 20:03:04 +0800

    fix var name

--- LeetCode/p145stackSimu.cpp
+++ LeetCode/p145stackSimu.cpp
@@ -20,40 +20,50 @@
  *     return;                          <- 4
  * }
  */
+/*
+ * void postOrderVisit(TreeNode *root) {
+ *     if (root == nullptr)             <- 0
+ *         return;
+ *     postOrderVisit(root->left);      <- 1
+ *     postOrderVisit(root->right);     <- 2
+ *     appendData(root->val);           <- 3
+ *     return;                          <- 4
+ * }
+ */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
         if (root == nullptr)
             return {};
         stack<pair<TreeNode *, int>> st;    // pair<data, PC>
-        pair<TreeNode *, int> FP = {root, 0};
+        pair<TreeNode *, int> SP = {root, 0};
         vector<int> result;
-        while (!(st.size() == 0 && FP.second == 4)) {
-            switch (FP.second) {
+        while (!(st.size() == 0 && SP.second == 4)) {
+            switch (SP.second) {
                 case 0:
-                    if (FP.first == nullptr) {
-                        FP = st.top();
+                    if (SP.first == nullptr) {
+                        SP = st.top();
                         st.pop();
                     } else {
-                        ++FP.second;
+                        ++SP.second;
                     }
                     break;
                 case 1:
-                    ++FP.second;
-                    st.push(FP);
-                    FP = {FP.first->left, 0};
+                    ++SP.second;
+                    st.push(SP);
+                    SP = {SP.first->left, 0};
                     break;
                 case 2:
-                    ++FP.second;
-                    st.push(FP);
-                    FP = {FP.first->right, 0};
+                    ++SP.second;
+                    st.push(SP);
+                    SP = {SP.first->right, 0};
                     break;
                 case 3:
-                    result.push_back(FP.first->val);
-                    ++FP.second;
+                    result.push_back(SP.first->val);
+                    ++SP.second;
                     break;
                 case 4:
-                    FP = st.top();
+                    SP = st.top();
                     st.pop();
                     break;
             }
