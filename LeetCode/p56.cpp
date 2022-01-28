72487e7011d28dd2b6895359f333deb45e13a7a8
KujouRinka <kujourinka@gmail.com>
2022-01-28 22:52:37 +0800

    p56

---
+++ LeetCode/p56.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
+        for (auto &interval: intervals) {
+            if (result.empty()) {
+                result.push_back(interval);
+            } else {
+                if (interval[0] <= result.back()[1]) {
+                    result.back()[1] = max(result.back()[1], interval[1]);
+                } else {
+                    result.push_back(interval);
+                }
+            }
+        }
+        return result;
+    }
+};
+
